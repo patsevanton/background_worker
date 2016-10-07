b594910e84fc0f13b65822eb2fa8c3642d9d48a3
Anton Patsev <patsev.anton@gmail.com>
2016-10-07 23:45:59 +0600

    Refactoring code to class

--- background_worker.py
+++ background_worker.py
@@ -1,29 +1,39 @@
 #!/usr/bin/env python2
 # -*- encoding: utf-8 -*-
+from functools import wraps
 from threading import Thread
 from threading import Timer
 import datetime
+
 now = datetime.datetime.now
 
-def background_worker(second):
-    def my_decorator(func):
-        def wrapper(*args, **kwargs):
-            def TaskManager():
+
+class background_worker(object):
+    def __init__(self, argument):
+        self.arg = argument
+        self.daemon = False
+
+    def __call__(self, func):
+        @wraps(func)
+        def decorated(*args, **kwargs):
+            def task_manager():
                 func()
-                t = Timer(second, TaskManager);
+                t = Timer(self.arg, task_manager)
                 t.start()
                 return t
-            func_hl = Thread(target=TaskManager, args=args, kwargs=kwargs)
+
+            func_hl = Thread(name='MyName',target=task_manager, args=args, kwargs=kwargs)
             func_hl.daemon = True
             func_hl.run()
+            self.daemon = True
             return func_hl
-        return wrapper
-    return my_decorator
+
+        return decorated
+
 
 @background_worker(10)
 def some_job():
     print 'job', now()
 
+
 some_job()
-
-
