7879174a4a19bf91f91a140f6c0b83f99b99e410
Anton Patsev <Patsev.Anton@gmail.com>
2016-10-07 16:19:39 +0600

    Change backround_worker

--- background_worker.py
+++ background_worker.py
@@ -1,18 +1,27 @@
 #!/usr/bin/env python2
 # -*- encoding: utf-8 -*-
 
+#!/usr/bin/env python2
+# -*- encoding: utf-8 -*-
+
 import datetime
 import time
 import threading
 now = datetime.datetime.now
 
 def background_worker(second):
-    def wrapper(func):
-        while True:
-            time.sleep(second)
-            threading.Thread(target=func())
-        return func()
-    return wrapper
+    def my_decorator(func):
+        def wrapper():
+            while True:
+                time.sleep(second)
+                func()
+            return func()
+
+        t = threading.Thread(target=wrapper)
+        t.daemon = True
+        t.start()
+        return wrapper
+    return my_decorator
 
 
 @background_worker(10)
@@ -20,3 +29,4 @@ def some_job():
     print 'job', now()
 
 some_job()
+
